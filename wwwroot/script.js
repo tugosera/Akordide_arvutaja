7796e4bd37f0e971a7bdeb465c95046150e5c2cf
opilane <opilane@HP-CZC2349HTR>
2025-11-20 16:56:19 +0200

    vivivi

---
+++ wwwroot/script.js
@@ -0,0 +1,12 @@
+﻿async function getChord() {
+    const chord = document.getElementById("chordSelect").value;
+
+    // Здесь правильный URL для твоего контроллера
+    const url = `/Chord?chords=${encodeURIComponent(chord)}&format=names`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    document.getElementById("result").innerText =
+        "Результат: " + data.result.join(", ");
+}
